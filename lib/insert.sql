124f6ddda3bc9619c6877e7921cbb3e78143440d
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-22 02:56:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1, )
+(1, "Mr. Chocolate", 19, "M", "red", "angry", 1)
+(2, "Rowdy", 3, "F", "red", "gentle", 1)
+(3, "Tabitha", 5, "M", "brown", "happy", 0)
+(4, "Sergeant Brown", 1, "M", "brown", "happy", 1)
+(5, "Melissa", 3, "F", "black", "grumpy", 0)
+(6, "Grinch", 7, "M", "black", "angry", 1)
+(7, "Wendy", 10, "M", "orange", "happy", 0)
+(8, nil, 10, "F", "brown", "sad", 1)
